31b54a6ba2f4ae77a0e8a47a5c748f2d051f5a30
jsontang <jsontang@webank.com>
2023-04-12 09:49:03 +0800

    fix: move data folder

--- dive_into_deep_learning/3-线性神经网络/007.py
+++ dive_into_deep_learning/3-线性神经网络/007.py
@@ -14,9 +14,9 @@ use_svg_display()
 # 通过ToTensor实例将图像数据从PIL类型变换成32位浮点数格式， # 并除以255使得所有像素的数值均在0〜1之间
 trans = transforms.ToTensor()
 mnist_train = torchvision.datasets.FashionMNIST(
-    root="../data", train=True, transform=trans, download=True)
+    root="../../data", train=True, transform=trans, download=True)
 mnist_test = torchvision.datasets.FashionMNIST(
-root="../data", train=False, transform=trans, download=True)
+root="../../data", train=False, transform=trans, download=True)
 
 print(len(mnist_train), len(mnist_test))
 
